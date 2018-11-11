170632ea02680e7883a9712e44f7f3c75afcf52e
AndreiB97 <andrei_birzan@yahoo.com>
2018-11-11 16:27:50 +0200

    update triangle

--- MiniCAD_Java/code/Triangle.java
+++ MiniCAD_Java/code/Triangle.java
@@ -28,10 +28,8 @@ public final class Triangle implements DrawableShape {
     }
 
     public Triangle(final int[] x, final int[] y, final int outline, final int fill) {
-        final int codingStyleMagicNumber3 = 3;
-        // :)
-        coord = new Point[codingStyleMagicNumber3];
-        for (int i = 0; i < codingStyleMagicNumber3; i++) {
+        coord = new Point[3];
+        for (int i = 0; i < 3; i++) {
             coord[i] = new Point(x[i], y[i]);
         }
         outlineColor = outline;
